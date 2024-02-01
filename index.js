599c0a26ef857ddca33473497de7c27175f63024
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-01 19:43:15 +0530

    server created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config();
 // console.log(process.env);
 console.log('welcome to node js');
-const express =require('express');
+// const express =require('express');
 const app=express();
 const port=4000;
 app.get('/',(req,res)=>{
