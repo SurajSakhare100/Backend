555753a99f82f133188f3ef1430cd8231ab47c24
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-01 20:01:23 +0530

    server created

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// require('dotenv').config();
-// console.log(process.env);
+require('dotenv').config();
+console.log(process.env);
 console.log('welcome to node js');
-// const express =require('express');
+const express =require('express');
+const serverless =require('serverless-http');
 const app=express();
+const router=express.Router()
 const port=4000;
-app.get('/',(req,res)=>{
+router.get('/',(req,res)=>{
     res.send('hello i am server');
 })
-app.get('/youtube',(req,res)=>{
+router.get('/youtube',(req,res)=>{
     res.send('hello i am youtube');
 })
 
-app.get('/linkdin',(req,res)=>{
+router.get('/linkdin',(req,res)=>{
     res.send('hello i am linkdin');
 })
-app.get('/google',(req,res)=>{
+router.get('/google',(req,res)=>{
     res.send('hello i am google');
 })
-app.get('/login',(req,res)=>{
+router.get('/login',(req,res)=>{
     res.send('<h1>login page</h1>');
 })
 app.listen(process.env.PORT,()=>{
     console.log(`port is start:${port}`);
 })
+app.use('/.netlify/function/api',router);
+module.exports.handler=serverless(app);
