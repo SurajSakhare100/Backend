45cf79ffc8deaf971e32e72c3a5ee396e2bc7673
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-01 20:35:03 +0530

    server netlify

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-require('dotenv').config();
-console.log(process.env);
-console.log('welcome to node js');
-const express =require('express');
-const serverless =require('serverless-http');
-const app=express();
-const router=express.Router()
-const port=4000;
-router.get('/',(req,res)=>{
-    res.send('hello i am server');
-})
-router.get('/youtube',(req,res)=>{
-    res.send('hello i am youtube');
-})
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
-router.get('/linkdin',(req,res)=>{
-    res.send('hello i am linkdin');
-})
-router.get('/google',(req,res)=>{
-    res.send('hello i am google');
-})
-router.get('/login',(req,res)=>{
-    res.send('<h1>login page</h1>');
-})
-app.listen(process.env.PORT,()=>{
-    console.log(`port is start:${port}`);
-})
-app.use('/.netlify/function/api',router);
-module.exports.handler=serverless(app);
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use('/.netlify/functions/', router);
+
+export const handler = serverless(api);
